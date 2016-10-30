762aaa0ac4858eafa753f3c97e45511868e6e7a5
manucharyanruben <manucharyanruben@gmail.com>
2016-10-29 20:08:46 -0800

    created

--- ScaryGhostApp/Program.cs
+++ ScaryGhostApp/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace ScaryGhostApp
 {
-    static class Program
+    static class  Program
     {
         /// <summary>
         /// The main entry point for the application.
